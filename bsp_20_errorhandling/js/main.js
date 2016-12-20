825d56f2ff05a10b807579bdf3a9b7f6aa294eb2
Marcus Konitzer <mk@konitzer-training.de>
2016-12-20 14:33:52 +0100

    errorhandling

---
+++ bsp_20_errorhandling/js/main.js
@@ -0,0 +1,32 @@
+window.onerror = function (msg) {
+    alert("Es ist ein Fehler aufgetreten. Bitte später noch einmal versuchen.");
+    console.dir(msg);
+};
+
+(function () {
+    "use strict";
+
+    function app(a) {
+
+        if (a === undefined) {
+            //throw "Fehlerhafter Datentyp";
+            throw new TypeError(38474);
+        }
+    }
+
+    try {
+        app();
+    } catch (e) {
+        console.log(e instanceof TypeError); // true
+        console.log(e.message);              // "Hello"
+        console.log(e.name);                 // "TypeError"
+        console.log(e.fileName);             // "someFile.js"
+        console.log(e.lineNumber);           // 10
+        console.log(e.columnNumber);         // 0
+        console.log(e.stack);                // "@Scratchpad/2:2:9\n"
+
+
+        var image = new Image();
+        image.src = "http://localhost:3000/error?id=" + e.message;
+    }
+}());
