67dedc0f7f46a96089d9559779327594c0a2808f
Marcus Konitzer <mk@konitzer-training.de>
2016-12-20 11:07:41 +0100

    Module

---
+++ bsp_15_module/js/lib/collection.js
@@ -0,0 +1,26 @@
+/*global
+ window
+ */
+
+// namespace pattern
+window.BSI = window.BSI || {};
+
+// module pattern
+window.BSI.collection = (function () {
+    "use strict";
+    var basket = [];
+
+    return Object.create({
+        addItem: function (item) {
+            basket.push(item);
+        },
+        getAllItems : function () {
+            return basket.slice();
+        }
+    }, {
+        version : {
+            value: "2.3.4"
+        }
+    });
+
+}());
