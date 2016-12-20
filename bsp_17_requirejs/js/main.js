d85ccf12280d404bd5a98aaf39fe8e7119ee02a3
Marcus Konitzer <mk@konitzer-training.de>
2016-12-20 13:26:00 +0100

    Module

--- bsp_17_requirejs/js/main.js
+++ bsp_17_requirejs/js/main.js
@@ -4,7 +4,7 @@
 (function () {
     "use strict";
 
-    require(["modul1"], function (basket) {
+    require(["modul1", "modul2"], function (basket, nochWas) {
         basket.addItem("haus");
         var b = basket.getAllItems();
 
---
+++ bsp_18_methodchaining/js/main.js
@@ -0,0 +1,44 @@
+(function () {
+    "use strict";
+
+    var obj = new Object();
+
+    /**
+     * Erstellt einen Kunden
+     * @param fn
+     * @constructor
+     */
+    var Customer = function (fn, customerId) {
+
+        // ES3
+        this.fullname = fn;
+
+        Object.defineProperties(this, {
+            cid: {
+                get: function () {
+
+                    return customerId;
+                },
+                set: function (nId) {
+                    customerId = nId;
+                }
+            },
+            sayHello: {
+                value: function () {
+                    console.log("Hallo");
+                    return this; // Method Chaining
+                }
+            }
+        });
+        console.log(this);
+    };
+
+    var p1 = new Customer("hans", "3242344");
+
+    p1
+        .sayHello()
+        .sayHello()
+        .sayHello();
+
+
+}());
