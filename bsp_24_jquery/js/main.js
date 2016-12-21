42c37b6dc2c01d34bc85c6198de5a996c1df1cc0
Marcus Konitzer <mk@konitzer-training.de>
2016-12-21 13:47:30 +0100

    jquery

---
+++ bsp_24_jquery/js/main.js
@@ -0,0 +1,56 @@
+/*global
+ $
+ */
+// jQuery
+$(document).ready(function () {
+    "use strict";
+
+// DOM-Elemente finden
+    $("#box1").css({
+        backgroundColor: "red"
+    });
+    $("div:eq(2)").css({
+        backgroundColor: "red"
+    })
+
+// DOM-Elemente erstellen
+    $("<h1>Inhalt</h1>", {id: "main"}).appendTo("body");
+    $("h1").before("<h2>Überschrift</h2>");
+
+// DOM-Manipulieren
+// Events
+    $("#btn1").on("click", function () {
+        $("#box1").css({
+            backgroundColor: "green"
+        })
+        ;
+    });
+
+// Animationen
+
+    $("#box2").on("mouseenter", function (e) {
+        $(this)
+            .hide(500)
+            .show(200)
+            .slideDown(100)
+            .slideUp(300);
+    });
+
+    $("#box3").on("mouseenter", function (e) {
+        console.log(this);
+        $(this).animate({
+            width: "400px"
+        }, 300);
+    });
+
+    $("#box3").on("mouseleave", function (e) {
+        console.log(this);
+        $(this).animate({
+            width: "150"
+        }, 300);
+    });
+
+// Ajax
+
+
+});
