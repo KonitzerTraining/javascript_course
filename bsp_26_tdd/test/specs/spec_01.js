28b1f54e8e726a8872642bcccf6b5dfd2f635119
Marcus Konitzer <mk@konitzer-training.de>
2016-12-21 14:57:22 +0100

    jquery

---
+++ bsp_26_tdd/test/specs/spec_01.js
@@ -0,0 +1,18 @@
+/*global
+describe xdescribe it xit expect
+
+ */
+(function () {
+    "use strict";
+
+    describe("Baustein", function () {
+
+// Spec
+        xit("should work", function () {
+
+            // erwartung - matcher
+            expect(0).toBe(1);
+        });
+    });
+
+}());
