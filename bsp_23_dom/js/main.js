a8a2f7a932cb5d882adc197f877c8d6b4a3c9490
Marcus Konitzer <mk@konitzer-training.de>
2016-12-21 11:42:38 +0100

    dom

--- bsp_23_dom/js/main.js
+++ bsp_23_dom/js/main.js
@@ -1,16 +1,33 @@
 (function () {
     "use strict";
 
-    //
+    var daten = [
+        {
+            "id": "234523",
+            "fullname": "Hans Müller"
+        },
+        {
+            "id": "123432",
+            "fullname": "Peter Mayer"
+        },
+        {
+            "id": "235345",
+            "fullname": "Dieter Bohlen"
+        }
+    ];
     //var ausgabeElement = document.getElementById("ausgabe");
     var target = document.querySelector("#ausgabe");
 
     var originalTemplate = target.innerHTML;
-    var template = originalTemplate;
+    var ausgabe = "";
+    daten.forEach(function (kunde) {
 
-    template = template.replace("{{id}}", "ID aus Daten");
-    template = template.replace("{{fullname}}", "Hans oder so");
+        var template = originalTemplate;
+        template = template.replace("{{id}}", kunde.id);
+        template = template.replace("{{fullname}}", kunde.fullname);
+        ausgabe += template;
 
-    target.innerHTML = template;
+    });
+    target.innerHTML = ausgabe;
 
 }());
